feb9fd701e79ad15ce3cbde72367609f52f802b8
GamesLuk <lukas@meyer-zu-berstenhorst.de>
2026-04-19 22:37:41 +0200

    Remove debug log from connection handler
    Delete a leftover debug System.out.println and an explanatory comment in PlayerConnectionHadler.onSendConfiguration. The method still retrieves the player name from the GameProfile and calls FakePlayerAPI.deleteFakePlayer(server, playerName). Removes noisy console output and minor whitespace.

--- src/main/java/de/gamesluk/fakeplayerapi/api/PlayerConnectionHadler.java
+++ src/main/java/de/gamesluk/fakeplayerapi/api/PlayerConnectionHadler.java
@@ -8,11 +8,7 @@ public class PlayerConnectionHadler implements ServerConfigurationConnectionEven
 
     @Override
     public void onSendConfiguration(ServerConfigurationPacketListenerImpl handler, MinecraftServer server) {
-
-        // Du kannst zum Beispiel auf das Gameprofile zugreifen:
         String playerName = handler.getOwner().name(); // Get name from GameProfile during config
-        System.out.println("[DEBUG] Authentifiziert, aber noch nicht in der Welt: " + playerName);
-
         FakePlayerAPI.deleteFakePlayer(server, playerName);
     }
 }
